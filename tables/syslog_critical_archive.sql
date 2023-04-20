e0268303831adbd7fd4d205b56db6062f85adbfc
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-04-20 16:41:51 -0400

    Update syslog_critical_archive.sql

--- tables/syslog_critical_archive.sql
+++ tables/syslog_critical_archive.sql
@@ -1,6 +1,6 @@
-CREATE TABLE syslog_critical_archive
+CREATE TABLE userid_syslog_critical_archive
 (priority int, severity int, facility int, version int, event_timestamp bigint, hostname string,
-body string, appName string, procid string, messageid string, 
+body string, appName string, procid string, messageid string,
 structureddata struct<sdid:struct<eventid:string,eventsource:string,iut:string>>)
 STORED BY ICEBERG
-STORED AS PARQUET
+LOCATION 's3a://oss-uat2/iceberg/'
